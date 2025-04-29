8e8bf3a4dac0e844bdb858b218bd78d8a1ae3d43
RasmusVJS <rasmusjuulsvendsen@gmail.com>
2025-04-29 20:19:45 +0200

    Initial commit

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import tdd.Calculator;
+
+public class CalculatorTest {
+
+    @Test
+    public void test() {
+        Calculator calculator = new Calculator();
+        Assertions.assertEquals(calculator.add(1,2), 3);
+    }
+
+    @Test
+    public void test2() {
+        Calculator calculator = new Calculator();
+        Assertions.assertEquals(calculator.subtract(3,2), 1);
+    }
+
+    @Test
+    public void test3() {
+        Calculator calculator = new Calculator();
+        Assertions.assertEquals(calculator.multiply(2,3), 6);
+    }
+
+    @Test
+    public void test4() {
+        Calculator calculator = new Calculator();
+        Assertions.assertEquals(calculator.divide(6,3), 2);
+    }
+}
